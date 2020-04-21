3c990c061566743f6ab035583fd676839384fa4c
Guillaume Ferron <guillaume.ferron@gmail.com>
2020-04-21 11:11:46 +0200

    clean old code, add uuid display

--- DemoBluetooth/BluetoothViewController.swift
+++ DemoBluetooth/BluetoothViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 class BluetoothViewController: UIViewController {
-
+    
     init(bluetoothManager: BluetoothManager) {
         self.bluetoothManager = bluetoothManager
         super.init(nibName: nil, bundle: nil)
@@ -24,6 +24,7 @@ class BluetoothViewController: UIViewController {
         scrollView.translatesAutoresizingMaskIntoConstraints = false
         return scrollView
     }()
+    
     private lazy var stackView: UIStackView = {
         let stackView = UIStackView()
 
@@ -33,89 +34,46 @@ class BluetoothViewController: UIViewController {
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
-
-    private lazy var textFieldContainer: UIView = {
+    
+    private lazy var uuidDisplayContainer: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
-
-    private lazy var nameTextField: UITextField = {
-        let textField = UITextField()
-        textField.attributedPlaceholder = NSAttributedString(string: "Device name", attributes: [.foregroundColor: UIColor.lightGray])
-
-        textField.backgroundColor = .white
-        textField.borderStyle = .roundedRect
-        textField.layer.borderColor = UIColor.gray.cgColor
-        textField.layer.borderWidth = 1
-        textField.layer.cornerRadius = 8
-        textField.textColor = .black
-        textField.keyboardType = .default
-        textField.returnKeyType = .done
-        textField.clearButtonMode = .whileEditing
-        textField.translatesAutoresizingMaskIntoConstraints = false
-        return textField
+    
+    private lazy var uuidDisplay: UITextView = {
+        let data = UITextView()
+        data.text = bluetoothManager.uuid
+        data.isScrollEnabled = false // required
+        data.translatesAutoresizingMaskIntoConstraints = false // required
+        return data
     }()
-
-    private lazy var advertisingButtonContainer: UIView = {
+    
+    private lazy var covidStartButtonContainer: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
 
         return view
     }()
 
-    private lazy var advertisingButton: UIButton = {
+    private lazy var covidStartButton: UIButton = {
         let button = UIButton()
         button.setTitle("Start Advertising", for: .normal)
         button.backgroundColor = UIColor(red: 0/255, green: 122/255, blue: 255/255, alpha: 1)
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.addTarget(self, action: #selector(startAdvertising(sender:)), for: .touchUpInside)
+        button.addTarget(self, action: #selector(covidStart(sender:)), for: .touchUpInside)
 
         button.layer.cornerRadius = 8
 
         return button
     }()
-
-    private lazy var scanningButtonContainer: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-
-        return view
-    }()
-
-    private lazy var scanningButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("Start Scanning", for: .normal)
-        button.backgroundColor = UIColor(red: 0/255, green: 122/255, blue: 255/255, alpha: 1)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.addTarget(self, action: #selector(startScanning(sender:)), for: .touchUpInside)
-
-        button.layer.cornerRadius = 8
-
-        return button
-    }()
-
+    
     private var bluetoothManager: BluetoothManager
-
-    @objc private func startAdvertising(sender: UIButton!) {
-        nameTextField.resignFirstResponder()
-        guard let name = nameTextField.text?.trimmingCharacters(in: .whitespaces), !name.isEmpty else {
-            let alert = UIAlertController(title: "Invalid name", message: "Device name should not be empty", preferredStyle: .alert)
-
-            alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
-
-            self.present(alert, animated: true)
-            return
-        }
-
-        bluetoothManager.startAdvertising(with: name)
-    }
-
-    @objc private func startScanning(sender: UIButton) {
+    
+    @objc private func covidStart(sender: UIButton) {
         bluetoothManager.delegate = self
-        bluetoothManager.startScanning()
+        bluetoothManager.covidStart()
     }
-
 
 }
 
@@ -129,15 +87,12 @@ extension BluetoothViewController {
         view.addSubview(scrollView)
         scrollView.addSubview(stackView)
 
-        stackView.addArrangedSubview(textFieldContainer)
-        stackView.setCustomSpacing(16, after: textFieldContainer)
-        stackView.addArrangedSubview(advertisingButtonContainer)
-        stackView.setCustomSpacing(16, after: advertisingButtonContainer)
-        stackView.addArrangedSubview(scanningButtonContainer)
+        stackView.addArrangedSubview(uuidDisplayContainer)
+        stackView.setCustomSpacing(16, after: uuidDisplayContainer)
+        stackView.addArrangedSubview(covidStartButtonContainer)
 
-        textFieldContainer.addSubview(nameTextField)
-        advertisingButtonContainer.addSubview(advertisingButton)
-        scanningButtonContainer.addSubview(scanningButton)
+        uuidDisplayContainer.addSubview(uuidDisplay)
+        covidStartButtonContainer.addSubview(covidStartButton)
     }
 
     private func setupConstraints() {
@@ -157,25 +112,19 @@ extension BluetoothViewController {
         ])
 
         NSLayoutConstraint.activate([
-            nameTextField.leadingAnchor.constraint(equalTo: textFieldContainer.leadingAnchor, constant: 32),
-            nameTextField.trailingAnchor.constraint(equalTo: textFieldContainer.trailingAnchor, constant: -32),
-            nameTextField.topAnchor.constraint(equalTo: textFieldContainer.topAnchor),
-            nameTextField.bottomAnchor.constraint(equalTo: textFieldContainer.bottomAnchor)
-        ])
+              uuidDisplay.leadingAnchor.constraint(equalTo: uuidDisplayContainer.leadingAnchor, constant: 32),
+              uuidDisplay.trailingAnchor.constraint(equalTo: uuidDisplayContainer.trailingAnchor, constant: -32),
+              uuidDisplay.topAnchor.constraint(equalTo: uuidDisplayContainer.topAnchor),
+              uuidDisplay.bottomAnchor.constraint(equalTo: uuidDisplayContainer.bottomAnchor)
+          ])
 
         NSLayoutConstraint.activate([
-            advertisingButton.leadingAnchor.constraint(equalTo: advertisingButtonContainer.leadingAnchor, constant: 32),
-            advertisingButton.trailingAnchor.constraint(equalTo: advertisingButtonContainer.trailingAnchor, constant: -32),
-            advertisingButton.topAnchor.constraint(equalTo: advertisingButtonContainer.topAnchor),
-            advertisingButton.bottomAnchor.constraint(equalTo: advertisingButtonContainer.bottomAnchor)
+            covidStartButton.leadingAnchor.constraint(equalTo: covidStartButtonContainer.leadingAnchor, constant: 32),
+            covidStartButton.trailingAnchor.constraint(equalTo: covidStartButtonContainer.trailingAnchor, constant: -32),
+            covidStartButton.topAnchor.constraint(equalTo: covidStartButtonContainer.topAnchor),
+            covidStartButton.bottomAnchor.constraint(equalTo: covidStartButtonContainer.bottomAnchor)
         ])
-
-        NSLayoutConstraint.activate([
-            scanningButton.leadingAnchor.constraint(equalTo: scanningButtonContainer.leadingAnchor, constant: 32),
-            scanningButton.trailingAnchor.constraint(equalTo: scanningButtonContainer.trailingAnchor, constant: -32),
-            scanningButton.topAnchor.constraint(equalTo: scanningButtonContainer.topAnchor),
-            scanningButton.bottomAnchor.constraint(equalTo: scanningButtonContainer.bottomAnchor)
-        ])
+        
     }
 }
 
